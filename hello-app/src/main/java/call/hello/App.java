4fa3d5f8c7c209c6c5e792aaa52065f45b701244
EC2 Default User <ec2-user@ip-172-31-82-180.ec2.internal>
2022-09-17 15:57:29 +0000

    updated app.java

--- hello-app/src/main/java/call/hello/App.java
+++ hello-app/src/main/java/call/hello/App.java
@@ -3,7 +3,7 @@ package call.hello;
 public class App
 {
 
-    private final String message = "Hello World from Java Maven Project! I am modifiying my message";
+    private final String message = "Hello World from Java Maven Project!";
 
     public App() {}
 
